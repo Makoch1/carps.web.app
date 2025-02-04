b942b12098f892ccf4eacc547cad1f731ecfef9d
Hussye <enzo313yap@yahoo.com>
2025-02-05 00:04:12 +0800

    edit-profile

--- frontend/src/utils/getProfileData.js
+++ frontend/src/utils/getProfileData.js
@@ -2,7 +2,7 @@ import { getPosts } from './getPosts.js'
 
 const mockProfileData = [
     {
-        profilePicture: 'https://i.sstatic.net/l60Hf.png',
+        profilePicture: 'https://w7.pngwing.com/pngs/1000/665/png-transparent-computer-icons-profile-s-free-angle-sphere-profile-cliparts-free-thumbnail.png',
         description: 'Lorem ipsum I am a mock description.',
         posts: getPosts().slice(0, 2),
         savedPosts: getPosts().slice(2, 5),
