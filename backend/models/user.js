7d02aab06144b9c8c8342056793903ae30c9b60c
VinceMojica <vincelawrencemojica@gmail.com>
2025-03-31 22:36:05 +0800

    Changed the Admin Implementation

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,6 +6,7 @@ mongoose.connect("mongodb://localhost:27017/carps");
 const schema = {
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true }, // true if admin, false if not
     picture: { type: String },
     description: { type: String }
 };
