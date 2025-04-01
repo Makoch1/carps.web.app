c2af9128cc751d9400c4da4eb89a26d71d0db16c
Johann Salivio <saliviojohann@gmail.com>
2025-04-01 15:34:05 +0800

    Current User Context now persists through refreshes

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,15 +1,21 @@
 import { createContext, useState } from 'react';
 import { Navbar } from '../features/Navbar/Navbar.jsx'
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import axios from 'axios';
+import { BACKEND_BASE_URL } from '../utils/constants.js';
 
 export const Route = createRootRoute({
+    loader: () => {
+        return getAuth()
+    },
     component: RootComponent
 })
 
 export const CurrentUserContext = createContext(null);
 
 function RootComponent() {
-    const [currentUser, setCurrentUser] = useState();
+    const userData = Route.useLoaderData();
+    const [currentUser, setCurrentUser] = useState(userData);
 
     return (
         <CurrentUserContext.Provider
@@ -21,4 +27,13 @@ function RootComponent() {
             <Outlet />
         </CurrentUserContext.Provider>
     )
+}
+
+async function getAuth() {
+    try {
+        const res = await axios.get(`${BACKEND_BASE_URL}/auth`);
+        return await res.data;
+    } catch {
+        return null
+    }
 }
